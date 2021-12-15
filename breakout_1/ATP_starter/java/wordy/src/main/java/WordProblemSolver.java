26efa2bdc03e5113029622517b3bd254ee1e3715
giribabuthammisetty <giribabu.thammisetty@exactuals.com>
2021-12-15 13:48:41 -0500

    mob next [ci-skip] [ci skip] [skip ci]

--- breakout_1/ATP_starter/java/wordy/src/main/java/WordProblemSolver.java
+++ breakout_1/ATP_starter/java/wordy/src/main/java/WordProblemSolver.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /*
 
 Use the IDE shortcuts to assist in creating the class and methods.
 
 */
+class WordProblemSolver {
+
+    public int solve(String sentence){
+        sentence.split(" ");
+        List<String> tokens = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(sentence," ");
+
+        return 5;
+    }
+
+}

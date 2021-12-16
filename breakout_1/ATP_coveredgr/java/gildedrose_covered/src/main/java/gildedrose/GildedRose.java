f03458b06628610a9a8772e5bdc6c43e21b8d5ae
Bhavesh Thakker <bhavesh.thakker@exactuals.com>
2021-12-15 19:16:07 -0500

    mob next [ci-skip] [ci skip] [skip ci]

--- breakout_1/ATP_coveredgr/java/gildedrose_covered/src/main/java/gildedrose/GildedRose.java
+++ breakout_1/ATP_coveredgr/java/gildedrose_covered/src/main/java/gildedrose/GildedRose.java
@@ -9,14 +9,13 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
+            boolean isAgedOrBackStage = items[i].name.equals("Aged Brie")
+                    || items[i].name.equals("Backstage passes to a TAFKAL80ETC concert");
+            boolean notSulfurusAndQualityGreaterThan0 = items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros");
+            if (!isAgedOrBackStage && notSulfurusAndQualityGreaterThan0) {
+                items[i].quality = items[i].quality - 1;
+            }
+            if(isAgedOrBackStage) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 

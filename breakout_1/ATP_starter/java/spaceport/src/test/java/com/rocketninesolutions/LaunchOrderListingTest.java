f5e34ca85b731bf9f46b4ca9dcc12f622b8b9322
George Constantine <GConstantine88@gmail.com>
2021-12-17 04:45:57 +1100

    mob next [ci-skip] [ci skip] [skip ci]

--- breakout_1/ATP_starter/java/spaceport/src/test/java/com/rocketninesolutions/LaunchOrderListingTest.java
+++ breakout_1/ATP_starter/java/spaceport/src/test/java/com/rocketninesolutions/LaunchOrderListingTest.java
@@ -3,9 +3,19 @@ package com.rocketninesolutions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LaunchOrderListingTest {
+
+    private LaunchInfo createLaunchInfo(String destination)
+    {
+        LaunchInfo launchInfo = new LaunchInfo(UUID.randomUUID());
+        launchInfo.setDestination(destination);
+        //Add other fields here
+        return launchInfo;
+    }
 
     //TODO - Use the Stub Recipe to test that launches are sorted correctly
     @Test
@@ -17,8 +27,11 @@ public class LaunchOrderListingTest {
         SpaceportDepartureBoard board = new SpaceportDepartureBoard(provider);
         // Exercising this behavior happens during construction of the System Under Test
 
+        //provider.getCurrentLaunches().add();
+
+
         // Step 5. Verify the results are sorted correctly
-        Assertions.fail("TODO - test the info sorting behavior");
+        //Assertions.fail("TODO - test the info sorting behavior");
         assertEquals(-1, board.getLaunchList().get(0).getDestination().compareTo(board.getLaunchList().get(1).getDestination()));
     }
 }

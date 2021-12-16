7341e24ac067b20b841341527d102846c01df28d
jstafford <CatMouse2017!>
2021-12-16 12:06:35 -0600

    mob next [ci-skip] [ci skip] [skip ci]

--- breakout_1/ATP_starter/java/spaceport/src/test/java/com/rocketninesolutions/LaunchOrderListingTest.java
+++ breakout_1/ATP_starter/java/spaceport/src/test/java/com/rocketninesolutions/LaunchOrderListingTest.java
@@ -3,16 +3,18 @@ package com.rocketninesolutions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LaunchOrderListingTest {
 
-    private LaunchInfo createLaunchInfo(String destination)
+    private LaunchInfo createLaunchInfo(String destination, LocalDateTime dateTime)
     {
         LaunchInfo launchInfo = new LaunchInfo(UUID.randomUUID());
         launchInfo.setDestination(destination);
+        launchInfo.setTime(dateTime);
         //Add other fields here
         return launchInfo;
     }
@@ -23,8 +25,8 @@ public class LaunchOrderListingTest {
         // Step 1. Create LaunchInfoProviderStub (that implements ISpacelineLaunchInfoProvider)
         LaunchedFlightsTest.MockLaunchInfoProvider provider = new LaunchedFlightsTest.MockLaunchInfoProvider();
 
-        provider.getCurrentLaunches().add(createLaunchInfo("test2"));
-        provider.getCurrentLaunches().add(createLaunchInfo("test1"));
+        provider.getCurrentLaunches().add(createLaunchInfo("test2", LocalDateTime.now()));
+        provider.getCurrentLaunches().add(createLaunchInfo("test1", LocalDateTime.now()));
         // Step 2 & 3 & 4. Create SUT - SpaceportDepartureBoard, using Constructor Injection
         SpaceportDepartureBoard board = new SpaceportDepartureBoard(provider);
         // Exercising this behavior happens during construction of the System Under Test
@@ -35,5 +37,24 @@ public class LaunchOrderListingTest {
         //Assertions.fail("TODO - test the info sorting behavior");
         assertEquals("test1", board.getLaunchList().get(0).getDestination());
 				assertEquals("test2", board.getLaunchList().get(1).getDestination());
+    }
+
+    @Test
+    public void LaunchesAre_SortedByTime_TimesAreOrdered() {
+        // Step 1. Create LaunchInfoProviderStub (that implements ISpacelineLaunchInfoProvider)
+        LaunchedFlightsTest.MockLaunchInfoProvider provider = new LaunchedFlightsTest.MockLaunchInfoProvider();
+
+        provider.getCurrentLaunches().add(createLaunchInfo("test1", LocalDateTime.of(2021, 10, 10, 10, 10)));
+        provider.getCurrentLaunches().add(createLaunchInfo("test1", LocalDateTime.of(2021, 10, 10, 10, 10)));
+        // Step 2 & 3 & 4. Create SUT - SpaceportDepartureBoard, using Constructor Injection
+        SpaceportDepartureBoard board = new SpaceportDepartureBoard(provider);
+        // Exercising this behavior happens during construction of the System Under Test
+
+
+
+        // Step 5. Verify the results are sorted correctly
+        //Assertions.fail("TODO - test the info sorting behavior");
+        assertEquals("test1", board.getLaunchList().get(0).getDestination());
+        assertEquals("test1", board.getLaunchList().get(1).getDestination());
     }
 }
